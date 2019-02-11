77b3e9612b0fb07704463915b0d520f5fda1d2d2
sridatha <sridathatumati@gmail.com>
2019-02-11 02:34:25 +0000

    task4

---
+++ lab5-task4.py
@@ -0,0 +1,9 @@
+class Time(object):
+    """represents the time of the day"""
+time=Time()
+time.hour=11
+time.minute=59
+time.second=30
+def print_time(time):
+    print("%.2d:%.2d:%.2d"%(time.hour,time.minute,time.se$
+print_time(time)
